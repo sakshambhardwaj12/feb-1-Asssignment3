155a25aed81040faf48418984f6daa3aadd5f00b
saksham Upadhyay <saksham.upadhyay@tyutee.com>
2021-02-03 17:03:01 +0530

    Add project files.

---
+++ Asssign3/Eligible.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asssign3
+{
+    public class Eligible
+    {
+        private int maths;
+        private int physics;
+        private int chemistry;
+        public Eligible(int physics,int chemistry,int maths)
+        {
+            this.physics = physics;
+            this.chemistry = chemistry;
+            this.maths = maths;
+        }
+        public void eligibility()
+        {
+            int total;
+            total = maths + physics + chemistry;
+            if(maths>=65 && physics>=55 && chemistry>=50)
+            {
+                if(total>=180)
+                {
+                    Console.WriteLine("The student is eligible for admission");
+                }
+                else
+                {
+                    Console.WriteLine("Not eligible");
+                }
+            }
+            else if(maths+physics>=140)
+            {
+                Console.WriteLine("The student is eligible for admission");
+            }
+            else
+            {
+                Console.WriteLine("Not eligible");
+            }
+        }
+    }
+}
---
+++ Asssign3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Asssign3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter marks in physics");
+            int physics = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter marks in chemistry");
+            int chemistry = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter marks in maths");
+            int maths = int.Parse(Console.ReadLine());
+
+            Eligible e = new Eligible(physics,chemistry,maths);
+            e.eligibility();
+
+        }
+    }
+}
